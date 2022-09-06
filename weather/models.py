fc58c54b226f20582d8be27fec642513e6cbaaff
Dan Yu Liang <dyuliang19@gmail.com>
2022-09-05 23:27:26 -0400

    Updated with .get() retrieving JSON

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    """Creates a table in database that has a column called name"""
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        """ Shows actual city name on dashboard """
+        return self.name
+
+    #class Meta:
+    #    """Shows the plural of city as cities instead"""
+    #    plural_name = 'cities'
+
+# In order to make these changes in the database, run
+# python manage.py makemigrations
+# python manage.py migrate
--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+import requests
+from .models import City
 
 
 # Create your views here.
 def index(req):
-    return render(req, 'weather/index.html')
+    url = 'https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=6a90eda12e2d1507afef5e3a63ff7e6d'
+
+    cities = City.objects.all()
+
+    weather_data = []
+    for city in cities:
+        city_weather = requests.get(url.format(city)).json()
+        # This will request the API data and convert the JSON to Python data types
+
+        weather_info = {
+            'city': city,
+            'temperature': city_weather['list'][0]['main']['temp'],
+            'description': city_weather['list'][0]['weather'][0]['description'],
+            'icon': city_weather['list'][0]['weather'][0]['icon']
+        }
+
+        weather_data.append(weather_info)
+
+    context = {'weather': weather_data}
+
+    return render(req, 'weather/index.html', context)
